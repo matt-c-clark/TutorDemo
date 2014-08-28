8a0ec9123f42e6c5de6e8d4dbf409ed3c42c75ea
mattclark <matthew_c_clark@cox.net>
2014-08-27 20:46:17 -0700

    Views pretty much done

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Find a Private Tutor | FindMyTutor' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About Us | FindMyTutor' });
+});
+
+/* GET student sign up page. */
+router.get('/signup', function(req, res) {
+  res.render('signup', { title: 'Become a Student | FindMyTutor' });
+});
+
+/* GET tutor sign up page. */
+router.get('/tutor-signup', function(req, res) {
+  res.render('tutor-signup', { title: 'Become a Tutor | FindMyTutor' });
+});
+
+/* GET sign in page. */
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Sign In| FindMyTutor' });
 });
 
 module.exports = router;
